547a4afefaf2bd85d42391f6004db5b70630f9ca
Bryce Handerson <bryce.handerson@gmail.com>
2013-09-30 23:59:38 -0700

    submitted

--- sps/interpreter.py
+++ sps/interpreter.py
@@ -3,7 +3,6 @@ import re
 import sys
 
 pattern = '/?[a-zA-Z][a-zA-Z0-9_]*|[-]?[0-9]+|[}{]|%.*|[^\t\n ]'
-names = re.compile('/[a-zA-Z0-9?].*')
 # /? has a slash in front
 # * 0 or more
 # token can be right or left brace
@@ -13,9 +12,7 @@ names = re.compile('/[a-zA-Z0-9?].*')
 # A simple program
 opstack = []
 dicstack = [{}]
-gstack = []
-exstack = []
-
+count = 0
 
 brackets = {
         "{": 1,
@@ -27,10 +24,9 @@ brackets = {
         "<": 1,
         ">": -1,
         }
-
 demoProg = '''
 /fact {
-    1 dict begin
+    dictz begin
     /x exch def
         x 0 eq
         {1}
@@ -38,7 +34,7 @@ demoProg = '''
     ifelse
     end
 } def
-'''
+9 fact '''
 
 def parse(s):
     """Given a string, return the tokens it contains"""
@@ -85,8 +81,11 @@ def pnot(a):
         return error()
 def dup(a):
     a.append(a[-1])
-def exch(a):
-    a[-1], a[-2] = a[-2], a[-1]
+def exch():
+    if len(opstack)>1:
+        opstack[-2], opstack[-1] = opstack[-1], opstack[-2]
+        return
+    sys.exit()
 def pop(a):
     a.pop()
 def dictz():
@@ -97,23 +96,26 @@ def ptop(a):
     print(a.pop())
 
 def defined(d):
-	for x in dictStack:
-		if d in x.keys():
-			return x[d]
-		else:
-			return None
+    for x in dictStack:
+        if d in x.keys():
+            return x[d]
+        else:
+            return None
 
 
 def pfindmatch(tokens, pos):
     tempstack = []
     count = 1
-    while pos < len(tokens) and count > 0:
+    while True:
         a = tokens[pos]
-        pos +=1
-        if a in brackets.keys():
-            count += brackets[a]
-        else:
-            tempstack.append(a)
+        pos = pos + 1
+        if a == '}':
+            count = count -1
+            if count == 0:
+                break
+        elif a =='{':
+            count = count + 1
+        tempstack.append(a)
     return tempstack, pos
 
 def printStacks():
@@ -123,17 +125,13 @@ def printStacks():
     print("***dicstack***")
     for x in reversed(dicstack):
         print(x)
-    print("***gstack***")
-    for x in reversed(gstack):
-        print(x)
-    print("***exstack***")
-    for x in reversed(exstack):
-        print(x)
 
 def error(s):
     print("you received a(n) %s error. Whooops\n" % s)
     print("opstack: %s\n" % opstack)
     print("dicstack: %s\n" % dicstack)
+    sys.exit()
+
 ops = {
         "add": padd,
         "sub": psub,
@@ -147,14 +145,12 @@ ops = {
         "and": pand,
         "or": por,
         "not": pnot,
-        }
-# command line use: pass the filename as the first command-line argument
-if __name__ == "__main__":
-    #fn = sys.argv[1]
-    #print (parseFile(open(fn, 'r')))
-    #tokens = parse("3 2 add 2 add 10 add (string one)(take two) false stack")
-    tokens = parse("1 2 3 {/x 4 def} stack")
-    print(tokens)
+}
+
+def pseval(tokens):
+    global opstack
+    global dicstack
+    tempstack = []
     pos = 0
     while pos < len(tokens):
         tok = tokens[pos]
@@ -164,7 +160,7 @@ if __name__ == "__main__":
         except:
             if tok in ops.keys() and len(opstack)>1:
                 op = ops[tok]
-                opstack[-3:] = [op(opstack[-2],opstack[-1])]
+                opstack[-2:] = [op(opstack[-2],opstack[-1])]
             elif tok in brackets.keys():
                 code, pos = pfindmatch(tokens, pos)
                 opstack.append(code)
@@ -175,17 +171,86 @@ if __name__ == "__main__":
                 opstack.append(False)
             elif tok == 'pop':
                 opstack.pop()
+            elif tok == 'exch':
+                exch()
             elif tok == 'clear':
                 opstack = []
             elif tok == 'stack':
                 printStacks()
             elif tok == '=':
                 print(opstack.pop())
-            elif tok == fun.match(tok):
-                opstack.append(token[1:])
+            elif tok[0] == '/':
+                opstack.append(tok[1:])
+            elif tok == 'if':
+                a = opstack.pop()
+                b = opstack.pop()
+                if b == True and type(a) == list:
+                    pseval(a)
+            elif tok == 'ifelse':
+                a = opstack.pop()
+                b = opstack.pop()
+                c = opstack.pop()
+                if type(a) == list and type(b) == list and c == True:
+                    pseval(b)
+                else:
+                    pseval(a)
             elif tok == 'def':
-                if len(opstack)>1 and type(opstack[-2])==str:
-                    tempD = {opstack[-2]:opstack[-1]}
-                    opstack[-2:] = []
-            else:
-                error(tok)
+                dicstack[-1][opstack.pop()] = opstack.pop()
+            elif tok == 'begin':
+                if type(tempstack) == dict:
+                    dicstack.append(tempstack)
+            elif tok == 'end':
+                dicstack.pop()
+            elif tok == 'dictz':
+                tempstack = dictz()
+            elif type(tok) == list:
+                print("token: %s" % tok)
+                pseval(tok)
+            else: # variable name
+                found = False
+                for x in dicstack:
+                    if tok in x.keys():
+                        found = True
+                        if type(x[tok]) == list:
+                            pseval(x[tok])
+                            continue
+                        opstack.append(x[tok])
+                        continue
+                if not found:
+                    error(tok)
+
+# command line use: pass the filename as the first command-line argument
+if __name__ == "__main__":
+    #fn = sys.argv[1]
+    #print (parseFile(open(fn, 'r')))
+    #tokens = parse("3 2 add 2 add 10 add (string one)(take two) false stack")
+    #tokens = parse("/x 4 def /f { 1 3 add } def x f add =")
+    #tokens = parse("3 4 add /x {3 2 add} def x add =")
+    #tokens = parse("3 dict stack")
+    #tokens = parse("0 1 eq {3} if stack")
+    #tokens = parse("1 0 eq {3} {4} ifelse =")
+    #tokens = parse(" 7 3 4 add stack")
+    #tokens = parse("3 2 add stack")
+    #tokens = parse("1 dictz /x 4 def begin /y 5 def ")
+    #tokens = parse("/x 3 def")
+    #tokens = parse("9 /x exch def")
+    #tokens = parse(" 3 4 3 4 exch")
+    #tokens = parse(demoProg)
+    #tokens = parse("1 0 eq")
+    #tokens = parse(" 1 2 3 4 sub")
+    #tokens = parse("9 dictz begin /x exch def x 0 eq")
+    #tokens = parse("{1{2{3}}}")
+    pseval(tokens)
+    print("      opstack     ")
+    print("==================")
+    for x in opstack:
+        print(x)
+        print("------------------")
+    print("==================")
+    print("      dicstack    ")
+    print("==================")
+    for x in dicstack:
+        print(x)
+        print("------------------")
+    print("==================")
+
